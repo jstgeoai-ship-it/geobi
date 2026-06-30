3d8b7eaed0e924167f38014279ace740ee7a35bc
Nabila <nabilanahdatul@gmail.com>
2026-06-30 17:20:26 +0700

    penambahan 1234567

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -294,7 +294,7 @@
 
                 <br>
                 <span class="text-cyan-400">
-                    Jakarta SmartTax ooooo
+                    Jakarta SmartTax 12345678
                 </span>
 
             </h1>
