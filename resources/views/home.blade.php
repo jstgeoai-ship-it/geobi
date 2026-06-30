0c03b6cf7759e503988452d5b080f65c3a6cbd3d
yuafanatasha <yuafanatashaa11@gmail.com>
2026-06-30 15:53:01 +0700

    ini diubah yang yuafa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -290,7 +290,7 @@
             <!-- TITLE -->
             <h1 class="mt-8 text-3xl md:text-4xl font-bold leading-tight text-slate-900 dark:text-white transition-colors duration-300">
 
-                SmartMap Geospatial Business Intelligence WOIIIIIIII CPTN!!!!!
+                SmartMap Geospatial Business Intelligence ini yuafa yaaaaaa
 
                 <br>
                 <span class="text-cyan-400">
